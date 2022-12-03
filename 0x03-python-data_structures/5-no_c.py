3d24f5f0d52003d694d0fcec785bfe0af89511b1
Mesfin2121 <medmesfin20@gmail.com>
2022-12-03 06:13:41 -0800

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+
     no_c_string = ""
     for i in range(len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
             no_c_string += my_string[i]
-            return (no_c_string)
+    return (no_c_string)
