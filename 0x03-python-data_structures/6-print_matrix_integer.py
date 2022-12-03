1f02697af95126b4a6f1863ef9f611357976d664
Mesfin2121 <medmesfin20@gmail.com>
2022-12-03 06:26:34 -0800

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    row_len = len(matrix)
+    col_len = len(matrix[0])
+    for i in range(row_len):
+        for j in range(col_len):
+            if j != col_len - 1:
+                print("{:d}".format(matrix[i][j]), end=' ')
+            else:
+                print("{:d}".format(matrix[i][j]), end='')
+        print("")
