61b35d386b3bf70fc1783b1552611a772d2a1801
Mesfin2121 <medmesfin20@gmail.com>
2022-12-19 09:42:40 -0800

    Task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             r_num += 1
         except (ValueError, TypeError):
             pass
-        print()
-        return r_num
+    print()
+    return r_num
