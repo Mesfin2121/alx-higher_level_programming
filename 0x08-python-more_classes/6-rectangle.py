ae883a16981e67b43820c791048c6d255f9aeaaf
Mesfin2121 <medmesfin20@gmail.com>
2023-01-02 10:22:27 -0800

    Task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,10 +5,13 @@
 class Rectangle():
     """Representation of a Rectangle"""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Instantiation with width and height"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
