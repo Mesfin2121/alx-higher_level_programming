6f804fa0ea8664bba332184ffb3486749543ffef
Mesfin2121 <medmesfin20@gmail.com>
2023-01-09 10:33:24 -0800

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""My list"""
+
+class MyList(list):
+    """class MyList that inherits from list"""
+
+    def print_sorted(self):
+        """prints the list (sorted)"""
+        print(sorted(self))
