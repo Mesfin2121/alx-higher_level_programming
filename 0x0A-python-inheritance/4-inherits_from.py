ae6b3afa1724f21f0fb6430544e5af47eda005be
Mesfin2121 <medmesfin20@gmail.com>
2023-01-09 10:50:52 -0800

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Only sub class of"""
+
+
+def inherits_from(obj, a_class):
+    """checks if an object is an instance of a class that inherited
+    (directly or indirectly) from the specified class"""
+    return (type(obj) is not a_classs and issubclass(type(obj), a_class))
