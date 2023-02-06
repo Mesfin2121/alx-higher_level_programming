06c7fdf8e4e995a0d93bb3740abae0571a2b414a
Mesfin2121 <medmesfin20@gmail.com>
2023-02-06 10:28:03 -0800

    Tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Write to a file"""
 
+
 def write_file(filename="", text=""):
     """writes a string to a text file (UTF8)
     and returns the number of characters written"""
