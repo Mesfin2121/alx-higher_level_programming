c5a6b01902760c597577ee66808e2b3ac5fae112
Mesfin2121 <medmesfin20@gmail.com>
2023-02-14 07:50:15 -0800

    Task 20 fix

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- displays the max temperature of each state (ordered by State name).
-SELECT `city`, AVG(`value`) AS `max_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC LIMIT 3;
+SELECT `city`, AVG(`value`) AS `max_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC `city` LIMIT 3;
