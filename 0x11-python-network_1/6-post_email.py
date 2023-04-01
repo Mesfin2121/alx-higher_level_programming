d0c8169b6bc53fd8660e6f890b8174ef4a557fbe
Mesfin2121 <medmesfin20@gmail.com>
2023-04-01 11:49:47 -0700

    Task 6 - 7

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""sends a request to the passed URL and displays the body of the response"""
+"""sends a POST request to the passed URL with the passed email as a parameter
+   displays the body of the response"""
 from sys import argv
-from requests import get
+from requests import post
 
 
 if __name__ == "__main__":
-    request = get(argv[1])
-    if request.status_code >= 400:
-        print("Error code: {}".format(request.status_code))
-    else:
-        print(request.text)
+    value = {"email": argv[2]}
+    request = post(argv[1], data=value)
+    print(request.text)
